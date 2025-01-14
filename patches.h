752db76e59278f380858ddc0657e08afb23a19d4
c6 <31777460+c6-dev@users.noreply.github.com>
2025-01-14 13:59:46 +0400

    refactor headers

--- patches.h
+++ patches.h
@@ -1,15 +1,4 @@
 #pragma once
-#include "netimmerse.h"
-#include "GameForms.h"
 
-const char* GetPackageTypeName(char type);
-TESPackage* __fastcall GetAIPackageHook(Actor* actor);
-char* __fastcall GetMapMarker(TESObjectREFR* thisObj, UInt16 mapMarkerType);
-void GetMapMarkerHook();
-void __fastcall SetTreeFullLODToINISetting(TESObjectCELL* cell);
-void MarkNode(NiNode* node);
-void __stdcall MarkPlayerBones();
-void __fastcall SetCellImageSpaceHook(TESObjectCELL* cell, void* edx, TESImageSpace* imageSpace);
-void uGridsLoadingCrashHook();
 void WritePatches();
 void WriteEditorPatches();
